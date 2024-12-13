b77f7f7539803d532d229204421be4b5af293b43
sunjian <sunjxan@gmail.com>
2024-12-13 10:32:39 +0800

    .gitignore

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 # echo "cpu:"
-# nvcc -O2 -std=c++17 -Xcompiler -Wall cpu_1.cu -o a && ./a
-# nvcc -O2 -std=c++17 -Xcompiler -Wall -DUSE_DP cpu_1.cu -o a && ./a
-# nvcc -O2 -std=c++17 -Xcompiler -Wall cpu_2.cu -o a && ./a
-# nvcc -O2 -std=c++17 -Xcompiler -Wall -DUSE_DP cpu_2.cu -o a && ./a
+# nvcc -O2 -std=c++17 -Xcompiler -Wall cpu_1.cu -o a.out && ./a.out
+# nvcc -O2 -std=c++17 -Xcompiler -Wall -DUSE_DP cpu_1.cu -o a.out && ./a.out
+# nvcc -O2 -std=c++17 -Xcompiler -Wall cpu_2.cu -o a.out && ./a.out
+# nvcc -O2 -std=c++17 -Xcompiler -Wall -DUSE_DP cpu_2.cu -o a.out && ./a.out
 # echo ""
 echo "cuda:"
-nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr cuda_1.cu -o a && ./a
-nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr -DUSE_DP cuda_1.cu -o a && ./a
-nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr cuda_2.cu -o a && ./a
-nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr -DUSE_DP cuda_2.cu -o a && ./a
-nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr cuda_3.cu -o a && ./a
-nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr -DUSE_DP cuda_3.cu -o a && ./a
+nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr cuda_1.cu -o a.out && ./a.out
+nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr -DUSE_DP cuda_1.cu -o a.out && ./a.out
+nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr cuda_2.cu -o a.out && ./a.out
+nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr -DUSE_DP cuda_2.cu -o a.out && ./a.out
+nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr cuda_3.cu -o a.out && ./a.out
+nvcc -O2 -std=c++17 -Xcompiler -Wall --expt-relaxed-constexpr -DUSE_DP cuda_3.cu -o a.out && ./a.out
